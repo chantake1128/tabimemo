a25eb6bfa64b19fc7bd25717ec2cf37e01019445
chantake1128 <chantake1128@gmail.com>
2024-08-30 14:48:44 +0900

    viewの変更

--- db/migrate/20240829055816_create_users.rb
+++ db/migrate/20240829055816_create_users.rb
@@ -1,7 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-
+      t.string :nickname, nill: false
+      t.string :email,    nill: false, unique: true
+      
       t.timestamps
     end
   end
