fffda2d7159beea79482e222b8518158c35fb3d2
chantake1128 <chantake1128@gmail.com>
2024-09-01 01:38:10 +0900

    テーブル設定の修正

--- db/migrate/20240829052515_create_landmarks.rb
+++ db/migrate/20240829052515_create_landmarks.rb
@@ -1,6 +1,7 @@
 class CreateLandmarks < ActiveRecord::Migration[7.0]
   def change
     create_table :landmarks do |t|
+      t.string  :name,           null: false
       t.integer :prefecture_id,  null: false
       t.string  :city,           null: false
       t.string  :street_address, null: false
