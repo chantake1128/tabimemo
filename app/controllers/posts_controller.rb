e3f8a4d3e51a7b093d3e41d95c63a3f2f50b4ea6
chantake1128 <chantake1128@gmail.com>
2024-09-03 19:05:11 +0900

    旅行情報投稿機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,23 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_post, only:[:show]
   def index
     @posts = Post.includes(:user, :landmark).all
   end
 
   def new
+    @post = Post.new
+    @landmarks = Landmark.all
   end
 
   def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to @post, notice: 'Post was successfully created.'
+    else
+      @landmarks = Landmark.all
+      render :new
+    end
   end
 
   def show
@@ -17,6 +27,10 @@ class PostsController < ApplicationController
 
   def set_post
     @post = Post.includes(:user, :landmark).find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:image, :title, :body,:status_id, :category_id, :score_id, :landmark_id, :user_id)
   end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,10 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :landmark
   has_one_attached :image
+
+  validates :title,       presence: true
+  validates :body,        presence: true
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :status_id,   numericality: { other_than: 1, message: "can't be blank"}
+  validates :score_id,    numericality: { other_than: 1, message: "can't be blank"}
 end
