41a5dce61c0f4690bf8162ffc9e0920e57939bf6
chantake1128 <chantake1128@gmail.com>
2024-09-06 20:04:24 +0900

    いいね機能の修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
   resources :posts do
-    resource :likes, only: [:create, :destroy]
+    resources :likes, only: [:create, :destroy]
   end
   resources :landmarks, only: [:new, :create]
 end
